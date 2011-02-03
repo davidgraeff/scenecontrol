c28e6b98b42a3b6f9faea46c77752d9dcfe5bff2
davidgraeff <david.graeff@web.de>
2011-02-03 06:46:55 +0100

    QJson: Remove windows specific dll keywords because we are not building a library version.

--- shared/qjson/qjson_export.h
+++ shared/qjson/qjson_export.h
@@ -23,13 +23,7 @@
 #include <QtCore/qglobal.h>
 
 #ifndef QJSON_EXPORT
-# if defined(QJSON_MAKEDLL)
-   /* We are building this library */
-#  define QJSON_EXPORT Q_DECL_EXPORT
-# else
-   /* We are using this library */
-#  define QJSON_EXPORT Q_DECL_IMPORT
-# endif
+#  define QJSON_EXPORT
 #endif
 
 #endif
