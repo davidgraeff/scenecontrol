92830c55cbd4d3e7bd5ff374690269b554917c94
davidgraeff <david.graeff@web.de>
2011-04-23 00:47:01 +0200

    Add jquery-ui for sliders and other widgets. Listview complete. plugincontroller: call plugin init function. plugin io sockets: add initial list view

--- plugins/anel_sockets/www/switch.js
+++ plugins/anel_sockets/www/switch.js
@@ -1,1 +1,27 @@
- 
+function InitPlugin(pluginid, sectionname, $section) {
+	var datamodel = modelstorage.getModel("anel.io.value");
+	var namemodel = modelstorage.getModel("anel.io.name");
+
+	if (datamodel && namemodel) {
+		function getName(key) {
+			var count = namemodel.count();
+			for(i=0;i<count;++i) {
+				var item = namemodel.getItem(i);
+				if (item.channel == key) return item.name;
+			}
+			return key;
+		}
+		
+		function itemHtmlFunction(data) {
+			return getName(data.channel) + ": "+data.value;
+		}
+		
+		function itemCreationFunction() {
+			return $("<li></li>");
+		}
+
+		var $rootelement = $("<ul></ul>");
+		var listview = new ListView($rootelement, itemHtmlFunction, itemCreationFunction, datamodel);
+		$section.append($rootelement);
+	}
+}
