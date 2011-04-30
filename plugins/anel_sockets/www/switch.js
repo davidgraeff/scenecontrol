a13226f093ab7ad877a9ae15737815b307ca3b54
davidgraeff <david.graeff@web.de>
2011-04-30 05:44:37 +0200

    Client: Service add dialog: List plugins

--- plugins/anel_sockets/www/switch.js
+++ plugins/anel_sockets/www/switch.js
@@ -5,39 +5,42 @@ function InitPlugin(pluginid, sectionname, $section) {
 	var $rootelement = $('<ul class="anelsockets"></ul>');
 	$section.append($rootelement);
 
+	$.getCss(pluginid+"/"+sectionname+".css");
+
+	function getName(key) {
+		var count = namemodel.count();
+		for(i=0;i<count;++i) {
+			var item = namemodel.getItem(i);
+			if (item.channel == key) return item.name;
+		}
+		return key;
+	}
+	
+	function itemChangeFunction(domitem, modelitem) {
+		if (modelitem.value)
+			domitem.removeClass("anelsockets_deactivated").addClass("anelsockets_activated");
+		else
+			domitem.removeClass("anelsockets_activated").addClass("anelsockets_deactivated");
+		return domitem.text(getName(modelitem.channel));
+	}
+	
+	function tooglevalue(key) {
+		sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"iovalue_toogle","channel":key});
+	}
+	
+	function itemCreationFunction(modelitem) {
+		var item = $('<li class="anelsockets"></li>');
+		item.key = modelitem.channel;
+		item.click( function() { tooglevalue(item.key); });
+		return item;
+	}
+
+	var listview = new AbstractView($rootelement, itemChangeFunction, itemCreationFunction);
+	
 	function modelsAvailable() {
 		if (!datamodel || !namemodel) return;
-		$.getCss(pluginid+"/"+sectionname+".css");
 
-		function getName(key) {
-			var count = namemodel.count();
-			for(i=0;i<count;++i) {
-				var item = namemodel.getItem(i);
-				if (item.channel == key) return item.name;
-			}
-			return key;
-		}
-		
-		function itemChangeFunction(domitem, modelitem) {
-			if (modelitem.value)
-				domitem.removeClass("anelsockets_deactivated").addClass("anelsockets_activated");
-			else
-				domitem.removeClass("anelsockets_activated").addClass("anelsockets_deactivated");
-			return domitem.text(getName(modelitem.channel));
-		}
-		
-		function tooglevalue(key) {
-			sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"iovalue_toogle","channel":key});
-		}
-		
-		function itemCreationFunction(modelitem) {
-			var item = $('<li class="anelsockets"></li>');
-			item.key = modelitem.channel;
-			item.click( function() { tooglevalue(item.key); });
-			return item;
-		}
-
-		var listview = new AbstractView($rootelement, itemChangeFunction, itemCreationFunction, datamodel);
+		listview.setModel(datamodel);
 	}
 	
 	function modelAvailable(event, modelid, modeldata) {
--- plugins/pulseaudio/www/control.js
+++ plugins/pulseaudio/www/control.js
@@ -2,64 +2,64 @@ function InitPlugin(pluginid, sectionname, $section) {
 	var $rootelement = $('<ul class="pulseaudiochannels"></ul>');
 	$section.append($rootelement);
 
+	$.getCss(pluginid+"/"+sectionname+".css");
+
+	function itemChangeFunction(domitem, modelitem) {
+		domitem.itemText.text(modelitem.sinkid);
+		domitem.itemSlider.slider("value", modelitem.volume*10000);
+		var options;
+		if ( !modelitem.mute ) {
+			options = {
+				label: "unmuted",
+				icons: {
+					primary: "ui-icon-volume-on"
+				}
+			};
+		} else {
+			options = {
+				label: "muted",
+				icons: {
+					primary: "ui-icon-volume-off"
+				}
+			};
+		}
+		domitem.itemMute.button( "option", options );
+		
+		return domitem;
+	}
+	
+	function changeChannel(sindid, volume) {
+		sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"pulsechannelvolume","volume":volume,"sindid":sindid});
+	}
+
+	function muteChannel(sindid, mute) {
+		sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"pulsechannelmute","mute":mute,"sindid":sindid});
+	}
+	
+	function itemCreationFunction(modelitem) {
+		var item = $('<li class="pulsechannel ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only"></li>');
+		var itemMute = $('<button class="channelmute" />');
+		var itemText = $('<span class="channelname" />');
+		var itemSlider = $('<div class="channelvalue" />');
+		item.itemText = itemText;
+		item.itemMute = itemMute;
+		item.itemSlider = itemSlider;
+		item.append(itemMute);
+		item.append(itemText);
+		item.append(itemSlider);
+		
+		itemMute.button({text: false,icons: {primary: "ui-icon-volume-off"}}).click(function() {
+			muteChannel(modelitem.sinkid, $( this ).text() != "muted");
+		});;
+		itemSlider.slider({min: 0, max: 10000, value: 0, orientation: 'horizontal'});
+		itemSlider.bind( "slide", function(event, ui) {changeChannel(modelitem.sinkid, ui.value/10000);});
+		return item;
+	}
+
+	var listview = new AbstractView($rootelement, itemChangeFunction, itemCreationFunction);
 	function modelAvailable(event, modelid, modeldata) {
 		if (!modeldata || (modelid && modelid != "pulse.channels")) return;
-
- 		$.getCss(pluginid+"/"+sectionname+".css");
-
-		function itemChangeFunction(domitem, modelitem) {
-			domitem.itemText.text(modelitem.sinkid);
-			domitem.itemSlider.slider("value", modelitem.volume*10000);
-			var options;
-			if ( !modelitem.mute ) {
-				options = {
-					label: "unmuted",
-					icons: {
-						primary: "ui-icon-volume-on"
-					}
-				};
-			} else {
-				options = {
-					label: "muted",
-					icons: {
-						primary: "ui-icon-volume-off"
-					}
-				};
-			}
-			domitem.itemMute.button( "option", options );
-			
-			return domitem;
-		}
-		
-		function changeChannel(sindid, volume) {
-			sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"pulsechannelvolume","volume":volume,"sindid":sindid});
-		}
-
-		function muteChannel(sindid, mute) {
-			sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"pulsechannelmute","mute":mute,"sindid":sindid});
-		}
-		
-		function itemCreationFunction(modelitem) {
-			var item = $('<li class="pulsechannel ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only"></li>');
-			var itemMute = $('<button class="channelmute" />');
-			var itemText = $('<span class="channelname" />');
-			var itemSlider = $('<div class="channelvalue" />');
-			item.itemText = itemText;
-			item.itemMute = itemMute;
-			item.itemSlider = itemSlider;
-			item.append(itemMute);
-			item.append(itemText);
-			item.append(itemSlider);
-			
-			itemMute.button({text: false,icons: {primary: "ui-icon-volume-off"}}).click(function() {
-				muteChannel(modelitem.sinkid, $( this ).text() != "muted");
-			});;
-			itemSlider.slider({min: 0, max: 10000, value: 0, orientation: 'horizontal'});
-			itemSlider.bind( "slide", function(event, ui) {changeChannel(modelitem.sinkid, ui.value/10000);});
-			return item;
-		}
-
-		var listview = new AbstractView($rootelement, itemChangeFunction, itemCreationFunction, modeldata);
+		listview.setModel(modeldata);
 	}
 	
 	$(modelstorage).bind('modelAvailable', modelAvailable);
