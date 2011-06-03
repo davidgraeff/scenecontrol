c4093949cac0101da5310d2a11da8eba6147e3a2
davidgraeff <david.graeff@web.de>
2011-06-03 02:27:55 +0200

    Client: Functions; mpd plugin ported

--- plugins/mpd/www/control.js
+++ plugins/mpd/www/control.js
@@ -1,74 +1,74 @@
-function RoomPlugin(pluginid, sectionname, $section) {
+function RoomcontrolPlugin(pluginid, sectionname) {
 	var that = this;
-	this.$rootelement = $('<div class="ui-widget-header ui-corner-all" style="width:80%;margin:auto;margin-bottom:1em" />');
+	this.status = new Ext.Toolbar();
 	
-	this.load = function() {
-		if (!that.$rootelement) return;
-		$section.append(that.$rootelement).append('<div class="ui-widget-header ui-corner-all" style="width:80%;margin:auto"><span id="mpdconnection"></span><span id="mpdcurrent"></span><span id="mpdtrack"></span></div>');
-		that.$rootelement.append($('<button>Vorheriger</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-seek-start" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdcmd","state":4});
-				})
-		);
-		that.$rootelement.append($('<button>Zurückspulen</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-seek-prev" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdposition","relative":1,"position_in_ms":-5000});
-				})
-		);
-		that.$rootelement.append($('<button>Start/Pause</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-play" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdcmd","state":0});
-				})
-		);
-		that.$rootelement.append($('<button>Stop</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-stop" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdcmd","state":2});
-				})
-		);
-		that.$rootelement.append($('<button>Vorspulen</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-seek-next" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdposition","relative":1,"position_in_ms":5000});
-				})
-		);
-		that.$rootelement.append($('<button>Nächster</button>').button({
-				text: false,
-				icons: { primary: "ui-icon-seek-end" }
-				}).click(function() {
-					sessionmanager.socket_write({"__type":"execute","__plugin":pluginid,"id":"mpdcmd","state":3});
-				})
-		);
-		
-		$(sessionmanager).bind('notification', that.notification);
+	this.card = new Ext.Panel({
+		defaults: {
+			xtype: 'button',
+			margin: 10
+		},
+		items: [
+			that.status,
+			{
+				text: 'Vorheriger',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdcmd","state":4});
+				}
+			},
+			{
+				text: 'Zurückspulen',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdposition","relative":1,"position_in_ms":-5000});
+				}
+			},
+			{
+				text: 'Start/Pause',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdcmd","state":0});
+				}
+			},
+			{
+				text: 'Stop',
+				ui: 'decline',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdcmd","state":2});
+				}
+			},
+			{
+				text: 'Vorspulen',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdposition","relative":1,"position_in_ms":5000});
+				}
+			},
+			{
+				text: 'Nächster',
+				handler : function() {
+					roomcontrol.SessionController.writeToServer({"__type":"execute","__plugin":"mpd","id":"mpdcmd","state":3});
+				}
+			}
+		]
+	});
+	
+	this.init = function() {
+		$(roomcontrol.SessionManager).bind('notification', that.notification);
 	}
 	
 	this.clear = function() {
-		that.$rootelement.remove();
-		delete that.$rootelement;
+		
 	}
-	
+
 	this.notification = function(event, data) {
-		if (data.__plugin != pluginid)
+		if (data.__plugin != "mpd")
 			return;
 
 		if (data.id == "connection.state") {
-			$('#mpdconnection').text((data.state==0)?"No server connected":"Connected");
+			that.status.setTitle((data.state==0)?"No server connected":"Connected");
 		} else
 		if (data.id == "playlist.current") {
-			$('#mpdcurrent').text(data.playlistid);
+			that.status.setTitle(data.playlistid);
 		} else
 		if (data.id == "track.info") {
-			$('#mpdtrack').text(data.trackname);
+			that.status.setTitle(data.trackname);
 		}
 	}
-
 }
