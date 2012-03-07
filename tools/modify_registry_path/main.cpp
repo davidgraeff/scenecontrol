877526c6551b00b902d212121f5f698f8cd5893d
davidgraeff <david.graeff@web.de>
2012-03-07 14:34:47 +0100

    tools: registry install path: fix

--- tools/modify_registry_path/main.cpp
+++ tools/modify_registry_path/main.cpp
@@ -1,6 +1,6 @@
 /*
     RoomControlServer. Home automation for controlling sockets, leds and music.
-    Copyright (C) 2010  David Gräff
+    Copyright (C) 2012  David Gräff
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+	Purpose: For windows only. Set install path in registry
 */
 
 #include <QSettings>
@@ -38,18 +39,18 @@ int main(int argc, char *argv[])
 	int iu = list.indexOf(QLatin1String("uninstall"));
 
 	if (ii>=0) {
-		QDir dir =  QDir::current();
-		dir.cdUp ();
+		//QDir dir =  QDir::current();
+		//dir.cdUp ();
 		QSettings s;
-		s.setValue(QLatin1String("path"), dir.currentPath());
+		s.setValue(QLatin1String("path"), list[ii+1]);
 		return 0;
 	} else if (iu >=0) {
 		QSettings s;
 		s.clear();
 	    return 0;
 	} else {
-			Sleep(2000);
 		    qDebug() << "Usage: --install path OR --uninstall";
+//			Sleep(2000);
 	}
 
     return 1;
